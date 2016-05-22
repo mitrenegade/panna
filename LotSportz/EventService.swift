a921c4c7c73f6c3bd66b144fb6dfcb71277674a8
Bobby <bobbyren@gmail.com>
2016-05-22 12:14:31 -0400

    updated createEvent signature; added transaction for joining event after creating event

--- LotSportz/EventService.swift
+++ LotSportz/EventService.swift
@@ -48,17 +48,51 @@ class EventService: NSObject {
         }
     }
     
-    func createEvent(eventDict eventDict: [NSObject: AnyObject]?) {
+    func createEvent(type: String = "soccer", place: String = "Boston Commons, Boston", time: NSDate = NSDate(), max_players: UInt = 1, info: String?) {
         print ("Create events")
         let eventRef = firRef.child("events") //firebaseRef.childByAppendingPath("events") // this references the endpoint lotsports.firebase.com/events/
         let newEventRef = eventRef.childByAutoId() // this generates an autoincremented event endpoint like lotsports.firebase.com/events/<uniqueId>
         
-        if eventDict == nil {
             // TEST: Demo on how to use event. eventDict should not be nil in production
-            newEventRef.setValue(["type": "soccer", "place": "Boston Commons, Boston MA", "time": NSDate().dateByAddingTimeInterval(3600*24*2).timeIntervalSince1970, "max_players": 20, "info": "This is a friendly pickup game"])
+        var params: [String: AnyObject] = ["type": type, "place": place, "time": time.dateByAddingTimeInterval(3600*24*2).timeIntervalSince1970, "max_players": max_players]
+        if info == nil {
+            params["info"] = info!
         }
-        else {
-            newEventRef.setValue(eventDict)
+        newEventRef.setValue(params)
+        self.joinEvent(eventRef: newEventRef, join: true)
+    }
+    
+    func joinEvent(eventRef eventRef: FIRDatabaseReference, join: Bool) {
+        // use transactions: https://firebase.google.com/docs/database/ios/save-data#save_data_as_transactions
+        // join: whether or not to join. Can use this method to leave an event
+       
+        eventRef.runTransactionBlock({ (currentData: FIRMutableData) -> FIRTransactionResult in
+            if currentData.value != nil, let uid = FIRAuth.auth()?.currentUser?.uid {
+                var event = currentData.value as! [String : AnyObject]
+                var participants : [String: Bool] // must be a dictionary
+                participants = event["participants"] as? [String: Bool] ?? [:]
+                if join {
+                    // add self to participants
+                    participants[uid] = true
+                }
+                else {
+                    // remove self from participants
+                    participants.removeValueForKey(uid)
+                }
+                event["participants"] = participants
+                
+                // Set value and report transaction success
+                currentData.value = event
+                
+                print("Joing event Success: \(event)")
+                return FIRTransactionResult.successWithValue(currentData)
+            }
+            return FIRTransactionResult.successWithValue(currentData)
+        }) { (error, committed, snapshot) in
+            if (error != nil) {
+                print("Join event failure: \(error)")
+                print(error?.localizedDescription)
+            }
         }
     }
 }
