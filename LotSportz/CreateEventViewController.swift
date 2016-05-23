7e4185dd4ab0c397cb8bb9173c7504661c5edbec
tstriss <tstriss@bu.edu>
2016-05-23 11:03:39 -0400

    WIP: UIPickerView in CreateEventViewController

--- LotSportz/CreateEventViewController.swift
+++ LotSportz/CreateEventViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import SWRevealViewController
 
-class CreateEventTableViewController: UITableViewController {
+class CreateEventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
     let options = ["Sport Type", "City", "Location", "Day", "Start Time", "End Time", "Max Players"]
-    
+    var sportTypes = ["Soccer", "Basketball", "Flag Football"]
+    let maxPlayers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    var pickerData = []
+
+    @IBOutlet var pickerView: UIPickerView!
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var menuButton: UIBarButtonItem!
     @IBOutlet var saveButton: UIBarButtonItem!
     
@@ -25,8 +30,14 @@ class CreateEventTableViewController: UITableViewController {
         }
         
         self.navigationItem.title = "Create Event"
-        self.tableView.tableFooterView = UIView()
-
+        pickerView = UIPickerView(frame: CGRectMake(0, 200, view.frame.width, 300))
+        pickerView.backgroundColor = .whiteColor()
+        
+        pickerView.showsSelectionIndicator = true
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +54,12 @@ class CreateEventTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
         return 2
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
             return options.count
@@ -60,8 +71,9 @@ class CreateEventTableViewController: UITableViewController {
     }
 
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        print("Reloaded rows of table")
         switch indexPath.section {
         case 0:
             let cell : DetailCell = tableView.dequeueReusableCellWithIdentifier("detailCell", forIndexPath: indexPath) as! DetailCell
@@ -83,7 +95,7 @@ class CreateEventTableViewController: UITableViewController {
 
     }
     
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0:
             return "Details"
@@ -94,7 +106,7 @@ class CreateEventTableViewController: UITableViewController {
         }
     }
     
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         
         switch indexPath.section {
         case 1:
@@ -105,40 +117,37 @@ class CreateEventTableViewController: UITableViewController {
     
     }
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        //TODO
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                print("Tapped sport types")
+                //Display sport types
+                pickerData = sportTypes
+                pickerView.hidden = false
+                pickerView.reloadAllComponents()
+                view.bringSubviewToFront(pickerView)
+    
+            default:
+                break
+            }
+        }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        print("Reloaded number of rows")
+        return pickerData.count
     }
-    */
-
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        print("Reloaded components")
+        return pickerData[row] as? String
     }
-    */
 
 }
