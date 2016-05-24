1ff3fa40f079dfb1b2b6058fe10575071ca636c3
tstriss <tstriss@bu.edu>
2016-05-24 07:14:27 -0400

    Code cleanup

--- LotSportz/JoinEventsTableViewController.swift
+++ LotSportz/JoinEventsTableViewController.swift
@@ -98,7 +98,7 @@ class JoinEventsTableViewController: UITableViewController {
         cell.labelFull.text = "You're going!" //To-Do: Add functionality whether or not event is full
         
         cell.labelAttendance.text = "10 Attending" //To-Do: "\(event.maxPlayers()) Attending"
-        cell.btnAction.tag = indexPath.row
+        cell.btnAction.tag = indexPath.row //tag uniquely identifies cell, and therefore, the event
         
         switch event.type() {
         case "Basketball":
@@ -127,51 +127,9 @@ class JoinEventsTableViewController: UITableViewController {
      override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
      let event = sortedEvents[indexPath.row]
      
+     //To-Do: Don't think we need this, as long as we can read in taps from join/cancel buttons
      }
+     */
      
-     
-     // Override to support conditional editing of the table view.
-     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-     if editingStyle == .Delete {
-     // Delete the row from the data source
-     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-     } else if editingStyle == .Insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-
 }
