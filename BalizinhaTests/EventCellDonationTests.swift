e7308d43e5e762911703bef82226e55c601576e4
Bobby <bobbyren@gmail.com>
2017-10-09 10:22:37 -0400

    added event button tests for future events

--- BalizinhaTests/EventCellDonationTests.swift
+++ BalizinhaTests/EventCellDonationTests.swift
@@ -24,16 +24,6 @@ class EventCellDonationTests: XCTestCase {
         event = nil
     }
     
-    func testEventName() {
-        setupPastEvent()
-//        XCTAssert(cell?.labelName.text == "PastEvent", "Event name appears correctly")
-    }
-    
-    func testEventPast() {
-        setupPastEvent()
-        XCTAssert(event?.isPast == true, "Event should be past")
-    }
-    
     func testActionButtonForPastEvent() {
         setupPastEvent()
         guard let event = event else { return }
@@ -52,9 +42,33 @@ class EventCellDonationTests: XCTestCase {
         XCTAssert(viewModel.buttonTitle(eventStatus: status) == "", "Past events not joined should not show anything")
     }
     
+    func testActionButtonForFutureEvent() {
+        setupFutureEvent()
+        guard let event = event else { return }
+        XCTAssert(event.isPast == false, "Event should not be past")
+        
+        // future event, user owned
+        var status = (event.isPast, true, false)
+        XCTAssert(viewModel.buttonTitle(eventStatus: status) == "Edit", "Past owned events should not show button")
+        
+        // future event, user joined
+        status = (event.isPast, false, true)
+        XCTAssert(viewModel.buttonTitle(eventStatus: status) == "Leave", "Future joined event should show leave action")
+        
+        // future event, user is not part of it
+        status = (event.isPast, false, false)
+        XCTAssert(viewModel.buttonTitle(eventStatus: status) == "Join", "Future event should show join action")
+    }
+    
     fileprivate func setupPastEvent() {
         event = Event()
-        let hours: Int = Int(arc4random_uniform(72))
-        event?.dict = ["name": "PastEvent", "endTime": (Date().timeIntervalSince1970 - Double(hours * 3600)) as AnyObject, "info": "Randomly generated event" as AnyObject]
+        let hours = 5
+        event?.dict = ["name": "PastEvent", "endTime": (Date().timeIntervalSince1970 - Double(hours * 3600)) as AnyObject]
+    }
+    
+    fileprivate func setupFutureEvent() {
+        event = Event()
+        let hours = 5
+        event?.dict = ["name": "FutureEvent", "endTime": (Date().timeIntervalSince1970 + Double(hours * 3600)) as AnyObject]
     }
 }
