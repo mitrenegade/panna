daec95e7e8600d19abc13658f1b64e599f07ad42
Bobby <bobbyren@gmail.com>
2017-11-01 07:49:41 -0400

    map shows user location, can update region

--- Balizinha/MapViewController.swift
+++ Balizinha/MapViewController.swift
@@ -18,17 +18,51 @@ class MapViewController: EventsViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
+    
+    private lazy var __once: () = {
+        LocationService.shared.startLocation(from: self)
+    }()
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let _ = __once
+    }
+    
+    var currentRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  currentRadius, currentRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        print("region changed")
-        
+        currentRadius = mapView.currentRadius
+        print("mapview: region changed with radius \(currentRadius)")
+    }
+
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print("mapview: user location changed")
+        let location = CLLocation(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        centerMapOnLocation(location: location)
+    }
+}
+
+extension MKMapView {
+    
+    func topCenterCoordinate() -> CLLocationCoordinate2D {
+        return self.convert(CGPoint(x: self.frame.size.width / 2.0, y: 0), toCoordinateFrom: self)
     }
     
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        print("user location changed")
+    var currentRadius: Double {
+        let centerLocation = CLLocation(latitude: centerCoordinate.latitude, longitude: centerCoordinate.longitude)
+        let topCenterCoordinate = self.topCenterCoordinate()
+        let topCenterLocation = CLLocation(latitude: topCenterCoordinate.latitude, longitude: topCenterCoordinate.longitude)
+        return centerLocation.distance(from: topCenterLocation)
     }
+    
 }
