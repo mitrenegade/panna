522da5699d7501e72309ab51e434f557569f90d7
Bobby <bobbyren@gmail.com>
2017-11-01 09:35:48 -0400

    added mapView to PlaceSearchViewController

--- Balizinha/PlaceResultsViewController.swift
+++ Balizinha/PlaceResultsViewController.swift
@@ -12,24 +12,46 @@ import MapKit
 class PlaceResultsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var matchingItems:[MKMapItem] = []
-    //var mapView: MKMapView? = nil
+    weak var mapView: MKMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func parseAddress(_ selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
 }
 
 extension PlaceResultsViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        //        guard let mapView = mapView,
-        
-        guard let searchBarText = searchController.searchBar.text else { return }
+        guard let mapView = mapView, let searchBarText = searchController.searchBar.text else { return }
         
         let request = MKLocalSearchRequest()
         request.naturalLanguageQuery = searchBarText
-        //        request.region = mapView.region
+        request.region = mapView.region
         let search = MKLocalSearch(request: request)
         search.start { response, _ in
             guard let response = response else {
@@ -52,7 +74,7 @@ extension PlaceResultsViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell")!
         let selectedItem = matchingItems[indexPath.row].placemark
         cell.textLabel?.text = selectedItem.name
-        cell.detailTextLabel?.text = ""
+        cell.detailTextLabel?.text = parseAddress(selectedItem)
         return cell
     }
 }
--- Balizinha/PlaceSearchViewController.swift
+++ Balizinha/PlaceSearchViewController.swift
@@ -12,12 +12,30 @@ import MapKit
 class PlaceSearchViewController: UIViewController {
 
     var searchController: UISearchController?
-    
+    @IBOutlet weak var mapView: MKMapView? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupSearch()
+        
+        self.navigationItem.leftBarButtonItem?.title = nil
+    }
+    
+    private lazy var __once: () = {
+        LocationService.shared.startLocation(from: self)
+    }()
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let _ = __once
+    }
+    
+    fileprivate func setupSearch() {
         let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "PlaceResultsViewController") as! PlaceResultsViewController
+        locationSearchTable.mapView = mapView
         searchController = UISearchController(searchResultsController: locationSearchTable)
         searchController?.searchResultsUpdater = locationSearchTable
         
@@ -30,5 +48,27 @@ class PlaceSearchViewController: UIViewController {
         searchController?.dimsBackgroundDuringPresentation = true
         definesPresentationContext = true
     }
+    
+    fileprivate var first: Bool = true
+    func centerMapOnLocation(location: CLLocation) {
+        var span = mapView?.region.span ?? MKCoordinateSpanMake(0.05, 0.05)
+        if first {
+            first = false
+            span = MKCoordinateSpanMake(0.05, 0.05)
+        }
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        mapView?.setRegion(region, animated: true)
+    }
 }
 
+extension PlaceSearchViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("mapview: region changed with span \(mapView.region.span)")
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print("mapview: user location changed")
+        let location = CLLocation(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        centerMapOnLocation(location: location)
+    }
+}
