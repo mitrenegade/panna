5e611ac12b9543e9854b0625d27a8faab356f3b2
Bobby <bobbyren@gmail.com>
2018-01-06 21:04:43 -0500

    fixed tutorial sizing for small phones

--- Balizinha/TutoriaPageViewController.swift
+++ Balizinha/TutoriaPageViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class TutoriaPageViewController: UIViewController {
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var labelSubtitle: UILabel!
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var imageView: UIImageView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        // this forces the imageView to be the correct width by the time containerView changes size due to autolayout. Otherwise, imageView doesn't get the update until the next layout
+        containerView.setNeedsUpdateConstraints()
+        containerView.layoutIfNeeded()
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
     }
     
