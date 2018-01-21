cc2a8d072ebff4114182f8041eeb0a6b62041e33
Bobby <bobbyren@gmail.com>
2018-01-21 17:31:24 -0500

    AccountViewController is a UIViewController with a tableview and a footer; top constraint is to the safe zone

--- Balizinha/AccountViewController.swift
+++ Balizinha/AccountViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import FBSDKLoginKit
 
-class AccountViewController: UITableViewController {
+class AccountViewController: UIViewController {
     
     var menuOptions: [String]!
     var service = EventService.shared
     var paymentCell: PaymentCell?
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +31,7 @@ class AccountViewController: UITableViewController {
             menuOptions = menuOptions.filter({$0 != "Payment options"})
         }
         
-        self.navigationItem.title = "Account"
+        navigationItem.title = "Account"
         listenFor(NotificationType.LocationOptionsChanged, action: #selector(self.reloadTableData), object: nil)
     }
 
@@ -40,18 +42,67 @@ class AccountViewController: UITableViewController {
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToEditPlayerInfo" {
+            if let nav = segue.destination as? UINavigationController, let controller = nav.viewControllers[0] as? PlayerInfoViewController {
+                controller.player = PlayerService.shared.current
+                controller.isCreatingPlayer = false
+            }
+        }
+    }
     
+    private func logout() {
+        print("LoginLogout: logout called, trying firAuth.signout")
+        try! firAuth.signOut()
+        EventService.resetOnLogout() // force new listeners
+        PlayerService.resetOnLogout()
+        OrganizerService.resetOnLogout()
+        FBSDKLoginManager().logOut()
+        self.notify(.LogoutSuccess, object: nil, userInfo: nil)
+    }
+    
+    // MARK: - Promotions
+    func addPromotion() {
+        guard let current = PlayerService.shared.current else { return }
+        let alert = UIAlertController(title: "Please enter a promo code", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Promo code"
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] (action) in
+            if let textField = alert.textFields?[0], let promo = textField.text {
+                print("Using promo code \(promo)")
+                PromotionService.shared.withId(id: promo, completion: { [weak self] (promotion, error) in
+                    if let promotion = promotion {
+                        print("\(promotion)")
+                        current.promotionId = promotion.id
+                        self?.tableView.reloadData()
+                        LoggingService.shared.log(event: LoggingEvent.AddPromoCode, message: "success", info: ["code":promo], error: nil)
+                    }
+                    else {
+                        self?.simpleAlert("Invalid promo code", message: "The promo code \(promo) seems to be invalid.")
+                        LoggingService.shared.log(event: LoggingEvent.AddPromoCode, message: "invalid", info: ["code":promo], error: error)
+                    }
+                })
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+}
+
+extension AccountViewController: UITableViewDataSource, UITableViewDelegate {
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuOptions.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         switch menuOptions[indexPath.row] {
         case "Push notifications":
@@ -111,7 +162,7 @@ class AccountViewController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: true)
        
         switch menuOptions[indexPath.row] {
@@ -143,51 +194,8 @@ class AccountViewController: UITableViewController {
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ToEditPlayerInfo" {
-            if let nav = segue.destination as? UINavigationController, let controller = nav.viewControllers[0] as? PlayerInfoViewController {
-                controller.player = PlayerService.shared.current
-                controller.isCreatingPlayer = false
-            }
-        }
-    }
-    
-    private func logout() {
-        print("LoginLogout: logout called, trying firAuth.signout")
-        try! firAuth.signOut()
-        EventService.resetOnLogout() // force new listeners
-        PlayerService.resetOnLogout()
-        OrganizerService.resetOnLogout()
-        FBSDKLoginManager().logOut()
-        self.notify(.LogoutSuccess, object: nil, userInfo: nil)
-    }
-    
-    // MARK: - Promotions
-    func addPromotion() {
-        guard let current = PlayerService.shared.current else { return }
-        let alert = UIAlertController(title: "Please enter a promo code", message: nil, preferredStyle: .alert)
-        alert.addTextField { (textField : UITextField!) -> Void in
-            textField.placeholder = "Promo code"
-        }
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] (action) in
-            if let textField = alert.textFields?[0], let promo = textField.text {
-                print("Using promo code \(promo)")
-                PromotionService.shared.withId(id: promo, completion: { [weak self] (promotion, error) in
-                    if let promotion = promotion {
-                        print("\(promotion)")
-                        current.promotionId = promotion.id
-                        self?.tableView.reloadData()
-                        LoggingService.shared.log(event: LoggingEvent.AddPromoCode, message: "success", info: ["code":promo], error: nil)
-                    }
-                    else {
-                        self?.simpleAlert("Invalid promo code", message: "The promo code \(promo) seems to be invalid.")
-                        LoggingService.shared.log(event: LoggingEvent.AddPromoCode, message: "invalid", info: ["code":promo], error: error)
-                    }
-                })
-            }
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        self.present(alert, animated: true)
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.1
     }
 }
 
