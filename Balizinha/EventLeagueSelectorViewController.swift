e54a38d1b66438a189c3f25ec3131289f10ab816
bobbyren <bobbyren@gmail.com>
2018-07-06 09:04:28 -0400

    displays no leagues in league selector if user is not an organizer

--- Balizinha/EventLeagueSelectorViewController.swift
+++ Balizinha/EventLeagueSelectorViewController.swift
@@ -45,8 +45,10 @@ class EventLeagueSelectorViewController: UIViewController {
                 return
             }
             
+            var organizerCount = 0
             for (leagueId, membership) in ids {
                 guard membership.isOrganizer else { continue }
+                organizerCount += 1
                 LeagueService.shared.withId(id: leagueId, completion: { [weak self] (league) in
                     if let league = league {
                         self?.playerLeagues.append(league)
@@ -55,6 +57,14 @@ class EventLeagueSelectorViewController: UIViewController {
                         }
                     }
                 })
+            }
+            if organizerCount == 0 {
+                DispatchQueue.main.async {
+                    self?.loading = false
+                    self?.reloadTableData()
+                    self?.simpleAlert("You're not an organizer", message: "You currently can't organize games for any leagues. Please contact the league owners to become an organizer.")
+                }
+                return
             }
         }
     }
