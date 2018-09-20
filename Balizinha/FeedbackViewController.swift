c6aee568dec18e3a5c8b757537a89d63747769f1
bobbyren <bobbyren@gmail.com>
2018-09-19 21:11:14 -0400

    hooked up storyboard correctly; added keyboard notifications

--- Balizinha/FeedbackViewController.swift
+++ Balizinha/FeedbackViewController.swift
@@ -15,9 +15,10 @@ class FeedbackViewController: UIViewController {
     @IBOutlet weak var inputSubject: UITextField!
     @IBOutlet weak var inputDetails: UITextView!
     
-    @IBOutlet weak var buttonSubmit: UIButton!
+    @IBOutlet weak var constraintBottomOffset: NSLayoutConstraint!
     
-    var isLeagueInquiry: Bool = false
+    fileprivate var isLeagueInquiry: Bool = false
+    fileprivate var shouldCancelInput: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,21 @@ class FeedbackViewController: UIViewController {
             inputSubject.text = "League inquiry"
             inputSubject.isUserInteractionEnabled = false
         }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit", style: .done, target: self, action: #selector(didClickSubmit(sender:)))
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        let keyboardNextButtonView = UIToolbar()
+        keyboardNextButtonView.sizeToFit()
+        keyboardNextButtonView.barStyle = UIBarStyle.black
+        keyboardNextButtonView.isTranslucent = true
+        keyboardNextButtonView.tintColor = UIColor.white
+        let cancel: UIBarButtonItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.done, target: self, action: #selector(cancelInput))
+        let flex: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        keyboardNextButtonView.setItems([flex, cancel], animated: true)
+        
+        inputSubject.inputAccessoryView = keyboardNextButtonView
+        inputDetails.inputAccessoryView = keyboardNextButtonView
     }
     
     @IBAction func didClickSubmit(sender: UIButton?) {
@@ -40,8 +56,8 @@ class FeedbackViewController: UIViewController {
             params["details"] = details
         }
         FirebaseAPIService().cloudFunction(functionName: "submitFeedback", method: "POST", params: params) { [weak self] (result, error) in
-            print("Feedback result \(result) error \(error)")
-            if let error = error as? NSError {
+            print("Feedback result \(String(describing: result)) error \(String(describing: error))")
+            if let error = error as NSError? {
                 self?.simpleAlert("Feedback failed", defaultMessage: "Could not submit feedback on \(subject)", error: error)
             } else {
                 self?.navigationController?.popToRootViewController(animated: true)
@@ -49,4 +65,45 @@ class FeedbackViewController: UIViewController {
         }
     }
     
+    fileprivate func keyboardWillShow(_ notification: Notification) {
+        let userInfo:NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardFrame:NSValue = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        
+        constraintBottomOffset.constant = keyboardHeight
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    fileprivate func keyboardWillHide(_ notification: Notification) {
+        constraintBottomOffset.constant = 20
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
+
+extension FeedbackViewController: UITextFieldDelegate {
+    @objc fileprivate func cancelInput() {
+        shouldCancelInput = true
+        view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        shouldCancelInput = false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard !shouldCancelInput else { return }
+        if textField == inputSubject {
+            inputDetails.becomeFirstResponder()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
