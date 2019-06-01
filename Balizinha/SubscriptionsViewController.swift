5c04fbbe7ec89eb3a252d931698a84a06c00eb0b
bobbyren <bobbyren@gmail.com>
2019-06-01 01:10:21 -0400

    StripePaymentService subscription calls take input parameters; fixed mock data

--- Balizinha/SubscriptionsViewController.swift
+++ Balizinha/SubscriptionsViewController.swift
@@ -21,38 +21,63 @@ class SubscriptionsViewController: UIViewController {
     }
     
     private func loadSubscriptions() {
+        var userId: String
         if AIRPLANE_MODE {
-            service.apiService = MockCloudAPIService(uniqueId: "!23", results: ["results": "success"])
+            userId = "123"
+            service.apiService = MockCloudAPIService(uniqueId: "123", results: ["1": ["leagueId": "123", "status": "active"]])
+        } else {
+            guard let id = PlayerService.shared.current.value?.id else { return }
+            userId = id
         }
-        service.loadSubscriptions()
+        
+        service.loadSubscriptions(userId: userId) { [weak self] results, error in
+            print("results \(results)")
+        }
     }
     
     @IBAction func didClickButton(_ sender: UIButton?) {
+        var userId: String
         if AIRPLANE_MODE {
-            service.apiService = MockCloudAPIService(uniqueId: "!23", results: ["results": "success"])
+            userId = "123"
+            service.apiService = MockCloudAPIService(uniqueId: "123", results: ["subscriptionId": "2", "subscription": ["leagueId": "123", "status": "active"]])
+        } else {
+            guard let id = PlayerService.shared.current.value?.id else { return }
+            userId = id
         }
-        service.createSubscription()
+        let leagueId = "abc"
+        let type = "owner"
+        service.createSubscription(userId: userId, leagueId: leagueId, type: type) { [weak self] results, error in
+            print("Received subscriptions \(results)")
+        }
     }
 }
 
+
+
 // TODO: move this into RenderPay
 extension StripePaymentService {
-    func loadSubscriptions() {
-        guard let userId = PlayerService.shared.current.value?.id else { return }
+    func loadSubscriptions(userId: String, completion: (([String:[String: Any]]?, Error?)->Void)?) {
         let params = ["userId": userId]
         
         apiService?.cloudFunction(functionName: "getSubscriptions", method: "POST", params: params) { (result, error) in
             print("Result \(result) error \(error)")
+            if let error = error {
+                completion?(nil, error)
+            } else {
+                completion?(result as? [String:[String: Any]], nil)
+            }
         }
     }
     
-    func createSubscription() {
-        guard let userId = PlayerService.shared.current.value?.id else { return }
+    func createSubscription(userId: String, leagueId: String, type: String, completion: (([String: Any]?, Error?)->Void)?) {
         let params = ["userId": userId, "leagueId": "123", "type": "owner"]
         apiService?.cloudFunction(functionName: "createSubscription", method: "POST", params: params) { (result, error) in
             print("Result \(result) error \(error)")
-            
-            self.loadSubscriptions()
+            if let error = error {
+                completion?(nil, error)
+            } else {
+                completion?(result as? [String: Any], nil)
+            }
         }
     }
 }
