e68220180444daa664f77cbb7709459093b09f29
bobbyren <bobbyren@gmail.com>
2019-06-16 22:23:52 -0400

    added title to dashboard

--- Balizinha/DashboardViewController.swift
+++ Balizinha/DashboardViewController.swift
@@ -31,7 +31,7 @@ class DashboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Dashboard"
     }
 }
 
