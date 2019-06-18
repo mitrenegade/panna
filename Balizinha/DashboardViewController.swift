ee15a21381c6e24652458afdc5fc7ad6b65ee98d
bobbyren <bobbyren@gmail.com>
2019-06-18 00:11:46 -0400

    added select league row

--- Balizinha/DashboardViewController.swift
+++ Balizinha/DashboardViewController.swift
@@ -13,6 +13,7 @@ import RxSwift
 
 enum DashboardMenuItem: String, CaseIterable {
     // player
+    case league = "League"
     case players = "Players"
     case events = "Events"
     case actions = "Actions"
@@ -33,12 +34,15 @@ class DashboardViewController: UIViewController {
         super.viewDidLoad()
 
         title = "Dashboard"
-        league = LeagueService.shared.ownerLeagues.first
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let controller = segue.destination as? ListViewController else { return }
         controller.league = league
+    }
+    
+    func promptForLeague() {
+        //        league = LeagueService.shared.ownerLeagues.first
     }
 }
 
@@ -55,19 +59,35 @@ extension DashboardViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = indexPath.row
-        let item = menuItems[row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = item.rawValue
-        cell.accessoryType = .disclosureIndicator
-        return cell
+        if row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LeagueSelectorCell", for: indexPath)
+            if let league = league {
+                cell.textLabel?.text = league.name ?? "Unnamed league"
+                cell.detailTextLabel?.text = "Click to switch leagues"
+            } else {
+                cell.textLabel?.text = "Click to select a league"
+                cell.detailTextLabel?.text = nil
+            }
+            return cell
+        } else {
+            let item = menuItems[row - 1]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = item.rawValue
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: true)
         
         let row = indexPath.row
-        let option = menuItems[row]
-        self.performSegue(withIdentifier: option.rawValue, sender: nil)
+        if row == 0 {
+            promptForLeague()
+        } else {
+            let option = menuItems[row - 1]
+            performSegue(withIdentifier: option.rawValue, sender: nil)
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
