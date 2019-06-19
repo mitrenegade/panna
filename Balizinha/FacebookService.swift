47ea423d3972a57bbf7f41b37f8f8cd017224932
bobbyren <bobbyren@gmail.com>
2019-06-19 01:52:18 -0400

    removed unnecessary dispatch async

--- Balizinha/FacebookService.swift
+++ Balizinha/FacebookService.swift
@@ -22,18 +22,16 @@ class FacebookService: NSObject {
             
             // update photoUrl if it doesn't already exist
             if let photoUrl = profile.imageURL(for: FBSDKProfilePictureMode.square, size: CGSize(width: 100, height: 100)) {
-                DispatchQueue.main.async {
-                    guard let data = try? Data(contentsOf: photoUrl) else { return }
-                    guard let image = UIImage(data: data) else { return }
-                    FirebaseImageService.uploadImage(image: image, type: .player, uid: player.id, completion: { (url) in
-                        if let name = profile.name {
-                            completion?(image, name, nil)
-                        }
-                        else if let name = profile.firstName {
-                            completion?(image, name, nil)
-                        }
-                    })
-                }
+                guard let data = try? Data(contentsOf: photoUrl) else { return }
+                guard let image = UIImage(data: data) else { return }
+                FirebaseImageService.uploadImage(image: image, type: .player, uid: player.id, completion: { (url) in
+                    if let name = profile.name {
+                        completion?(image, name, nil)
+                    }
+                    else if let name = profile.firstName {
+                        completion?(image, name, nil)
+                    }
+                })
             } else {
                 if let name = profile.name {
                     completion?(nil, name, nil)
