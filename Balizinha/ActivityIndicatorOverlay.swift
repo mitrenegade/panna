561987508c6f582528e28b063ca979015ca991e9
bobbyren <bobbyren@gmail.com>
2019-07-17 16:46:31 -0400

    activityIndicatorOverlay hides self on init

--- Balizinha/ActivityIndicatorOverlay.swift
+++ Balizinha/ActivityIndicatorOverlay.swift
@@ -31,6 +31,7 @@ class ActivityIndicatorOverlay: UIView {
             self.activityIndicator = activityIndicator
             
         }
+        hide()
 //        activityIndicator?.isHidden = true
     }
     
