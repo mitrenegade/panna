cbbd0f46907c7a541e2a7c3168103c889976ec2d
bobbyren <bobbyren@gmail.com>
2019-08-22 18:41:24 -0700

    added VenuesListViewController and VenueCell

---
+++ Balizinha/VenueCell.swift
@@ -0,0 +1,34 @@
+//
+//  VenueCell.swift
+//  Panna
+//
+//  Created by Bobby Ren on 8/22/19.
+//  Copyright © 2019 Bobby Ren. All rights reserved.
+//
+
+import UIKit
+import Balizinha
+
+class VenueCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView?
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var buttonMap: UIButton?
+    
+    var venue: Venue?
+    weak var presenter: UIViewController?
+
+    func configure(with venue: Venue?) {
+        guard let venue = venue else { return }
+        self.venue = venue
+        nameLabel.text = venue.name
+        addressLabel.text = venue.shortString ?? nil
+        
+        // TODO: load venue image
+    }
+    
+    @IBAction func didTapMap(_ sender: UIButton?) {
+        // TODO: open google map to show location
+    }
+}
+
